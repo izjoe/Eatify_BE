fa9c96255385202d60522524b2d2a88a35b102c5
shinny <nhungoc230805@gmail.com>
2025-11-22 16:02:32 +0700

    Remove deliveryAddress and phone from orderModel
    Removed deliveryAddress and phone fields from order model.

--- models/orderModel.js
+++ models/orderModel.js
@@ -13,8 +13,6 @@ const orderSchema = new mongoose.Schema(
     userID: { type: String, required: true },
     items: [orderItemSchema],
     totalPrice: { type: Number, required: true },
-    deliveryAddress: { type: String, required: true },
-    phone: { type: String, required: true },
     isPaid: { type: Boolean, default: false },
     orderStatus: {
       type: String,
